31210381b6b4822feb4d036f32ea4373955395cb
cweadock <cweadock@umich.edu>
2021-11-11 20:16:08 -0500

    completed slower, faster, skip, play, pause, mute

--- js/video.js
+++ js/video.js
@@ -1,11 +1,50 @@
-var video;
+var video = document.querySelector("#player1")
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+});
+
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down Video");
+	video.playbackRate *= 0.95;
+	console.log("Current speed: "+ video.playbackRate)
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed Up Video");
+	video.playbackRate *= 1.05;
+	console.log("Current speed: "+ video.playbackRate)
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 15 < video.duration){
+		video.currentTime+=15;
+	}
+	else{
+		video.currentTime = 0;
+	}
+	console.log("Current Location: "+video.currentTime)
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (video.muted == false){
+		video.muted = true;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+	else{
+		video.muted = false;
+		document.querySelector("#mute").innerHTML = "Mute";
+	}
+});
 
